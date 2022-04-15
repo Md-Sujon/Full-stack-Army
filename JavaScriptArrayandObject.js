318a49096f821a8ab947d09c3432b2357020bedb
Md-Sujon <mdsujon829562@gmail.com>
2022-04-16 00:49:56 +0600

    array

--- JavaScriptArrayandObject.js
+++ JavaScriptArrayandObject.js
@@ -1,6 +1,5 @@
 
 // utility of create a random id 
-
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
@@ -33,15 +32,86 @@ function uuidv4() {
 /**
  Array Operation
  -----------------
- * Easily Traverser
- * Filter
- * Delete
- * Update
- * create a new One
+ * 1. Easily Traverser
+ * 2. Filter
+ * 3. Delete (medium) [splice --> o(n), filter --> o(n)]
+ * 4. Update (medium) [push -->o(n)]
+ * 5. Create a new One (easy) [push --> o(1), unshift --> o(n)]
  */
 
 
+ //Create a new One
+ student.push({
+    id: '459b2b23-ed86-4ea4-ad8f-46e449547544',
+    name: 'Smita',
+    email: 'smita@gmail.com'
+  })
+
+//Update
+
+const idToUpdate = '0753523e-5deb-48a1-afae-1e29aafe9368';
+
+const updateData = {
+    name: 'Susmita',
+    email:'susmita@gmail.com'
+    
+}
+
+// Option One
+// This is not proper way
+
+const updateDataObj = student.find((item) => item.id == idToUpdate);
+updateDataObj.name = updateData.name;
+updateDataObj.email = updateData.email;
+ 
+
+
+//Option Two
+// This is not proper way
+
+ const upDateIndex = student.findIndex((item) => item.id == idToUpdate);
+student[upDateIndex] = {
+    id: idToUpdate,
+    ...updateData,
+} 
 
 
 
-  console.log(student);
+//Option Three
+// This is proper way
+const updateDataIndex = student.findIndex((item) => item.id == idToUpdate);
+student[updateDataIndex] = {
+    ...student[updateDataIndex],
+    ...updateData
+}
+//Update END
+
+//Delete
+student.splice(updateDataIndex, 1);
+console.log('Delete',student);
+
+// Traverser
+// ForEach, map, filter, every , reduce , some , find , findIndex
+
+//Option One 
+for (var i = 0; i <student.length; i++) {
+    console.log(student[i].name)
+    console.log(student[i].email)
+}
+
+// Option Two 
+
+for(let i in student){
+    console.log(student[i].name)
+}
+
+// Option three 
+
+for(let v of student){
+    console.log(v);
+}
+
+
+// console.log(updateDataObj);
+
+//   console.log(student);
