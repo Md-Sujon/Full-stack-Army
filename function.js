1ea12a5c913eeec85b9735cc9ac12b86efc2475d
Md-Sujon <mdsujon829562@gmail.com>
2022-04-24 01:49:03 +0600

    function

---
+++ function.js
@@ -0,0 +1,23 @@
+
+
+function name_of_function(){
+    //function body
+
+    //you can write any valid js code here
+
+    // return a result
+}
+
+//There are two steps
+
+//-Define a function
+
+// -Invoke a function
+  const c = 100;
+function test_function(a,b=5){
+    // const a = 10;
+    // const b = 20;
+    const result = a + b;
+    console.log(result);
+}
+test_function(c);
