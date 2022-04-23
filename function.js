318f418b03424ee6e62855da9afeb174a018bff3
Md-Sujon <mdsujon829562@gmail.com>
2022-04-24 02:20:09 +0600

    function

--- function.js
+++ function.js
@@ -1,4 +1,72 @@
 
+function sleep(name){
+console.log(`${name} is sleeping`);
+}
+
+
+function awake(name){
+    console.log(`${name} is taking hours`);
+}
+
+function eat(name, time){
+    console.log(`${name} is eat ${time}`);
+}
+
+
+function walk(name, destination){
+    console.log(`${name} is walking to ${destination}`);
+}
+
+
+function study(name){
+    console.log(`${name} is studying`);
+}
+
+function work(name){
+    console.log(`${name} working`);
+}
+
+function jobHolderLifecycle(name){
+    awake(name);
+    eat(name, "breakfast")
+    walk(name, 'Office');
+    work(name);
+    eat(name, "Lunch");
+    walk(name, 'home');
+    eat(name, "dinner");
+    sleep(name);
+}
+
+jobHolderLifecycle("sujon");
+console.log("----------------------------------------------------------------");
+jobHolderLifecycle("smita")
+console.log("----------------------------------------------------------------");
+jobHolderLifecycle("Rakib")
+console.log("----------------------------------------------------------------");
+jobHolderLifecycle("Robin")
+console.log("----------------------------------------------------------------");
+jobHolderLifecycle("Sumi")
+
+
+
+function StudentLifecycle(name){
+    awake(name);
+    eat(name, "breakfast")
+    study(name)
+    eat(name, "Lunch");
+    walk(name, 'home');
+    eat(name, "dinner");
+    sleep(name);
+}
+StudentLifecycle('Alifa')
+console.log("---------------------");
+StudentLifecycle('Akhi')
+console.log("---------------------");
+StudentLifecycle('Bilkish')
+
+
+
+
 
 function name_of_function(){
     //function body
